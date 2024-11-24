d3b84f6afe570dc64cd5bda4edbcda9e613ed073
seung-00 <seung-00@naver.com>
2024-11-24 13:15:08 +0900

    feat: Update deployment setup

--- infrastructure/deploy_nginx.sh
+++ infrastructure/deploy_nginx.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 REPO_PATH="/Users/holden/workspace/log/infrastructure"
-
 NGINX_CONF_DIR="/opt/homebrew/etc/nginx"
+NGINX_BIN="/opt/homebrew/bin/nginx"
 
+# Nginx 설정 파일 복사
 sudo cp "$REPO_PATH/nginx.conf" "$NGINX_CONF_DIR/nginx.conf"
+echo "Nginx configuration file has been updated."
 
-# Stop Nginx if it's running
-echo "Stopping Nginx if it's running..."
-sudo brew services stop nginx
+# Nginx 설정 문법 검사
+if ! sudo $NGINX_BIN -t; then
+    echo "Nginx configuration test failed. Please check the configuration file."
+    exit 1
+fi
 
-# Start Nginx
-echo "Starting Nginx..."
-sudo brew services start nginx
-
-echo "Nginx configuration has been updated and Nginx has been restarted."
+# Nginx 실행 여부 확인 및 처리
+if ps aux | grep -q '[n]ginx: master process'; then
+    echo "Nginx is running. Reloading Nginx..."
+    sudo $NGINX_BIN -s reload
+    echo "Nginx has been reloaded."
+else
+    echo "Nginx is not running. Starting Nginx..."
+    sudo brew services start nginx
+    echo "Nginx has been started."
+fi
 
