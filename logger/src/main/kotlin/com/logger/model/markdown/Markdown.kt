3edad77f42726ab7ac0235dd6909af2da6862c35
seung-00 <seung-00@naver.com>
2025-03-31 11:45:38 +0900

    feat: Markdown 도메인 클래스 수정

--- logger/src/main/kotlin/com/logger/model/markdown/Markdown.kt
+++ logger/src/main/kotlin/com/logger/model/markdown/Markdown.kt
@@ -1,6 +1,8 @@
 package com.logger.model.markdown
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -8,28 +10,40 @@ import java.time.ZonedDateTime
 data class Markdown(
   val id: String,
   val content: String,
-  val source: File,
+  val path: String,
+  val title: String,
+  val updatedAt: ZonedDateTime,
+  val createdAt: ZonedDateTime = ZonedDateTime.now(),
 ) {
-  val title: String by lazy { parseTitle() }
-
-  val updatedAt: ZonedDateTime by lazy { parseUpdatedAt() }
-
-  private fun parseTitle(): String {
-    return content
-      .split("\n")
-      .firstOrNull { it.startsWith("# ") }
-      ?.removePrefix("# ")
-      ?: "No Title"
-  }
-
-  private fun parseUpdatedAt(): ZonedDateTime {
-    val lastUpdated = source.lastModified()
-
-    return ZonedDateTime.ofInstant(Instant.ofEpochMilli(lastUpdated), ZoneId.systemDefault())
-  }
-
 
   companion object {
+    private fun parseTitle(file: File): String {
+      val firstH1 = file.readText()
+        .split("\n")
+        .firstOrNull { it.startsWith("# ") }
+        ?.removePrefix("# ")
+
+      return firstH1 ?: file.nameWithoutExtension
+    }
+
+    private fun parseUpdatedAt(file: File): ZonedDateTime {
+      val lastUpdated = file.lastModified()
+
+      return ZonedDateTime.ofInstant(Instant.ofEpochMilli(lastUpdated), ZoneId.systemDefault())
+    }
+
+    private fun parseCreatedAt(file: File): ZonedDateTime {
+      return try {
+        val path = file.toPath()
+        val attr: BasicFileAttributes = Files.readAttributes(path, BasicFileAttributes::class.java)
+        val createdMillis = attr.creationTime().toMillis()
+
+        ZonedDateTime.ofInstant(Instant.ofEpochMilli(createdMillis), ZoneId.systemDefault())
+      } catch (e: Exception) {
+        parseUpdatedAt(file)
+      }
+    }
+
     fun isMarkdown(file: File): Boolean {
       return file.extension == "md"
     }
@@ -37,28 +51,34 @@ data class Markdown(
     fun of(path: String): Markdown {
       val file = File(path)
 
-      if (!isMarkdown(file)) {
-        throw IllegalArgumentException("Markdown 파일이 아닙니다. (path: $path)") // TODO: exception 개선
+      require(isMarkdown(file)) {
+        "Markdown 파일이 아닙니다. (path: $path)" // TODO: exception 개선
       }
 
       return Markdown(
         id = file.nameWithoutExtension,
         content = file.readText(),
-        source = file,
+        path = path,
+        title = parseTitle(file),
+        updatedAt = parseUpdatedAt(file),
+        createdAt = parseCreatedAt(file),
       )
     }
 
     fun of(path: String, name: String): Markdown {
       val file = File("$path/$name.md")
 
-      if (!isMarkdown(file)) {
-        throw IllegalArgumentException("Markdown 파일이 아닙니다. (path: $path)") // TODO: exception 개선
+      require(isMarkdown(file)) {
+        "Markdown 파일이 아닙니다. (path: $path)" // TODO: exception 개선
       }
 
       return Markdown(
         id = file.nameWithoutExtension,
         content = file.readText(),
-        source = file,
+        path = path,
+        title = parseTitle(file),
+        updatedAt = parseUpdatedAt(file),
+        createdAt = parseCreatedAt(file),
       )
     }
   }
